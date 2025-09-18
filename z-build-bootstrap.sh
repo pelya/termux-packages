4034dd5427fb2ad1b94334863d39731b7c66e342
Sergii Pylypenko <x.pelya.x@gmail.com>
2025-09-18 15:14:20 +0300

    Updated build scripts

--- z-build-bootstrap.sh
+++ z-build-bootstrap.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-scripts/run-docker.sh scripts/build-bootstraps.sh --architectures aarch64 2>&1 | tee z-build-bootstrap.log
+{
+	scripts/run-docker.sh scripts/build-bootstraps.sh --architectures aarch64 2>&1
+	scripts/run-docker.sh scripts/generate-bootstraps.sh --architectures aarch64 2>&1
+} | tee z-build-bootstrap.log
