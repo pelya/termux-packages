3ec22685f55dbc2613c86804287400c2bfa420bd
Sergii Pylypenko <x.pelya.x@gmail.com>
2026-07-10 17:22:56 +0300

    Updated test build scripts

--- z-build-bootstrap.sh
+++ z-build-bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export PATH=/sbin:$PATH # Add path to apparmor tools on Debian 13
+
 {
 	[ -n "$1" ] && scripts/run-docker.sh scripts/build-bootstraps.sh 2>&1
 	#scripts/run-docker.sh scripts/generate-bootstraps.sh --architectures aarch64 2>&1
--- z-build.sh
+++ z-build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-scripts/run-docker.sh ./build-package.sh -a all -f command-not-found 2>&1 | tee z-build.log
+export PATH=/sbin:$PATH # Add path to apparmor tools on Debian 13
+
+scripts/run-docker.sh ./build-package.sh -a all zlib 2>&1 | tee z-build.log
+#scripts/run-docker.sh ./build-package.sh -a all -f command-not-found 2>&1 | tee z-build.log
