fab445b07014bafcd92952acf4c69dd4d9f75f74
Sergii Pylypenko <x.pelya.x@gmail.com>
2026-07-15 04:23:05 +0300

    Update build.sh

--- packages/command-not-found/build.sh
+++ packages/command-not-found/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/command-not-found
 TERMUX_PKG_DESCRIPTION="Suggest installation of packages in interactive shell sessions"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Yaksh Bariya <thunder-coding@termux.dev>"
-TERMUX_PKG_VERSION=3.5.0-pelya2
+TERMUX_PKG_VERSION=3.5.0-pelya3
 TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/pelya/command-not-found/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=f448df7e49ca94c89b5b49668e15eed6fb1c00b5456917af957dba4ded850496
+TERMUX_PKG_SHA256=8318489ab6d7fa6660bfd23c3a97cc9b972c7990f33435ca7b5d507b629839ac
 TERMUX_PKG_DEPENDS="libc++"
 
 termux_step_pre_configure() {
