00c578112a50c743d4754728f639579e4f42005d
Sergii Pylypenko <x.pelya.x@gmail.com>
2026-07-20 18:22:19 +0300

    More static libraries

--- scripts/buildorder.py
+++ scripts/buildorder.py
@@ -65,7 +65,7 @@ def parse_build_file_dependencies(path):
     "Extract the dependencies of a build.sh or *.subpackage.sh file."
     deps = parse_build_file_dependencies_with_vars(path, ('TERMUX_PKG_DEPENDS', 'TERMUX_PKG_BUILD_DEPENDS', 'TERMUX_SUBPKG_DEPENDS', 'TERMUX_PKG_DEVPACKAGE_DEPENDS'))
     # Hack to build X11 apps - libtool requires libx11.la for some reason, which is only present in libx11-static
-    for lib in ['x11', 'xcb', 'xdmcp', 'xext', 'xfixes', 'xft', 'xi', 'xt', 'xrandr', 'xtst']:
+    for lib in ['x11', 'xcb', 'xdmcp', 'xext', 'xfixes', 'xft', 'xi', 'xt', 'xrandr', 'xtst', 'xfont2', 'xinerama']:
         if f'lib{lib}' in deps and f'lib{lib}-static' not in deps:
             deps.add(f'lib{lib}-static')
     return deps
--- z-build.sh
+++ z-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PKGS='
-libxrender libxtst libxv
+xrdp
 '
 
 export PATH=/sbin:$PATH # Add path to apparmor tools on Debian 13
