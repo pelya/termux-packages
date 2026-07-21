633c77034c2ca0329d462ec28d921943ee688415
Sergii Pylypenko <x.pelya.x@gmail.com>
2026-07-21 10:25:12 +0300

    Added debug

--- z-build.sh
+++ z-build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 PKGS='
-xrdp
+pandoc
 '
 
 export PATH=/sbin:$PATH # Add path to apparmor tools on Debian 13
 
-scripts/run-docker.sh ./build-package.sh -I -a aarch64 $PKGS 2>&1 | tee z-build.log
+scripts/run-docker.sh ./build-package.sh -I -C -a i686 $PKGS 2>&1 | tee z-build.log
