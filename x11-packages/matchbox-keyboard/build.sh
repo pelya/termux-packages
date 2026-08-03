0c95331e472572eca8245eea23581cbfda73e296
Sergii Pylypenko <x.pelya.x@gmail.com>
2026-08-03 16:24:09 +0300

    matchbox-keyboard: updated dead link

--- x11-packages/matchbox-keyboard/build.sh
+++ x11-packages/matchbox-keyboard/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://www.yoctoproject.org/software-item/matchbox/
 TERMUX_PKG_DESCRIPTION="An on-screen virtual keyboard."
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=0.1.1
-TERMUX_PKG_REVISION=31
-TERMUX_PKG_SRCURL=https://git.yoctoproject.org/matchbox-keyboard/snapshot/matchbox-keyboard-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=dd3e9494a9499a3bf3017c8c1e6572a4e91deb20e219717db17c0977750b8bcb
+TERMUX_PKG_VERSION=0.2+git20231024
+TERMUX_PKG_REVISION=32
+TERMUX_PKG_SRCURL=https://deb.debian.org/debian/pool/main/m/matchbox-keyboard/matchbox-keyboard_0.2+git20231024.orig.tar.xz
+TERMUX_PKG_SHA256=c97de5d7e1d7e99baed1823555ee67d63a9e4cf4ecf3fb0cbe69e4af8c2524cf
 TERMUX_PKG_DEPENDS="libexpat, libfakekey, libpng, libx11, libxft, libxrender"
 TERMUX_PKG_RECOMMENDS="ttf-dejavu"
 
