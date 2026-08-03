ffb0654cd5c5b6253a38bee33783fc6a6e53d3e5
Sergii Pylypenko <x.pelya.x@gmail.com>
2026-08-03 18:01:41 +0300

    Script to count packages for source mirror

---
+++ z-count-pkgs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+shopt -s extglob
+LIBCNT="$(ls packages/lib*/build.sh x11-packages/lib*/build.sh root-packages/lib*/build.sh | wc -l)"
+
+for F in a b c d e f g h i j k l m n o p q r s t u v w x y z; do
+  echo "$F:" "$(ls packages/${F}*/build.sh x11-packages/$F*/build.sh root-packages/$F*/build.sh | wc -l)"
+done
+
+echo "===="
+
+for F in '@(0|1|2|3|4|5|6|7|8|9|a|b|c|d|e|f)' '@(g|h|i|j|k)' '@(l|m|n|o)' '@(p|q|r|s)' '@(t|u|v|w|x|y|z)'; do
+  CNT="$(ls packages/${F}*/build.sh x11-packages/$F*/build.sh root-packages/$F*/build.sh | wc -l)"
+  echo "$F" | grep 'l' >/dev/null && CNT=$(expr $CNT - $LIBCNT)
+  echo "$F: $CNT"
+done
+
+echo "lib: $LIBCNT"
+
+echo "===="
+
+for F in '@(0|1|2|3|4|5|6|7|8|9|a|b|c|d)' '@(e|f|g|h|i|j)' '@(k|l|m)' '@(n|o|p)' '@(q|r|s)' '@(t|u|v|w|x|y|z)'; do
+  CNT="$(ls packages/${F}*/build.sh x11-packages/$F*/build.sh root-packages/$F*/build.sh | wc -l)"
+  echo "$F" | grep 'l' >/dev/null && CNT=$(expr $CNT - $LIBCNT)
+  echo "$F: $CNT"
+done
+
+echo "lib: $LIBCNT"
