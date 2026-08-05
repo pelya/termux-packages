60d6a11c04b4c121a8f639d657117dfaea3106da
Sergii Pylypenko <x.pelya.x@gmail.com>
2026-08-05 16:23:07 +0300

    Switched repos to new directory layout

--- z-build.sh
+++ z-build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 PKGS='
-libmdbx
+vlan
 '
 
 export PATH=/sbin:$PATH # Add path to apparmor tools on Debian 13
 
-scripts/run-docker.sh ./build-package.sh -I -m  -a all $PKGS 2>&1 | tee z-build.log
+scripts/run-docker.sh ./build-package.sh -m -a all $PKGS 2>&1 | tee z-build.log
